c9bc41d79a9858b3fa22d7183c1a98cff29f001c
lucca <luccavilasboas@hotmail.com>
2022-07-20 01:56:57 -0300

    Create app instagram for dogs.

---
+++ src/Components/Feed/FeedModal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedModal = () => {
+  return (
+    <div>Feed Modal</div>
+  )
+}
+
+export default FeedModal
---
+++ src/Components/Login/LoginPasswordLost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPasswordLost = () => {
+  return (
+    <div>Login PasswordLost</div>
+  )
+}
+
+export default LoginPasswordLost
